24e816c259d4e1546d2e9eac8c635ff64a9b76d7
Tom Villegas <tvillega@dcc.uchile.cl>
2025-05-30 11:38:12 -0400

    feat: persistent level states

---
+++ autoloads/lobby_stats.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var unlocked_lvl_1 = false
+var beaten_lvl_1   = false
+
+var unlocked_lvl_2 = false
+var beaten_lvl_2   = false
+
+var unlocked_lvl_3 = false
+var beaten_lvl_3   = false
+
+var unlocked_lvl_4 = false
+var beaten_lvl_4   = false
--- scenes/levels/level_1.gd
+++ scenes/levels/level_1.gd
@@ -4,8 +4,15 @@ extends Node2D
 @onready var unlock_flying: Area2D = $UnlockFlying
 @onready var goose: CharacterBody2D = $Players/Goose
 
+func _ready() -> void:
+
+	if LobbyStats.beaten_lvl_1:
+		Debug.log("Already beaten")
+	
+
 func _on_unlock_flying_body_entered(body: Node2D) -> void:
 	#pass # Replace with function body.
 	Debug.log("Unlocked Jump")
-	PlayerStats.can_fly = true
+	PlayerStats.can_jump = true
+	LobbyStats.beaten_lvl_1 = true
 	get_tree().change_scene_to_file("res://scenes/lobby.tscn")
