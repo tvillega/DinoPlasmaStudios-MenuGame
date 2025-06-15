d90869043276bc60ca2c6e883abd334bc18ec55f
Tom Villegas <tvillega@dcc.uchile.cl>
2025-06-15 17:52:18 -0400

    feat: add flying unlockable

--- scenes/miniboss.gd
+++ scenes/miniboss.gd
@@ -64,5 +64,6 @@ func take_damage():
 	is_damaged = true
 	playback.travel("damage")
 	LobbyStats.beaten_lvl_2 = true
+	PlayerStats.can_fly = true
 	await get_tree().create_timer(0.5).timeout
 	queue_free()
